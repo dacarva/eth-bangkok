fb9460e9d7eedda03f89cea42a89a1de8984232c
bogos <andre.neyra@gmail.com>
2024-11-16 09:43:12 -0500

    remove json parser

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import json from "@rollup/plugin-json";
 
 export default defineConfig({
-  plugins: [react(), json()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  assetsInclude: ["**/*.json"],
 });
